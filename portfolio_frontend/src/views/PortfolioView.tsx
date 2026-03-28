274bc9a129ea2a5b1f611b8f1fa7bdc812e65185
Cruschcell <administrator@CL5-12.cl5.lab>
2026-03-28 15:31:16 +0800

    Modified Move Smart Contract Portfolio

--- portfolio_frontend/src/views/PortfolioView.tsx
+++ portfolio_frontend/src/views/PortfolioView.tsx
@@ -98,7 +98,7 @@ const PortfolioView = () => {
   // ==========================================================================
   // STATE MANAGEMENT
   // ==========================================================================
-  const objectId = "0xa3343391df96e28464499f4c209d51bf209c07392fdeea97bfeee59e7550f020";
+  const objectId = "0xa7ecfcc29972868500d8192fb3261920cf783028a21a21912e5ef3a569a5f540";
   
   // Network state - default to testnet, can be changed if needed
   const [currentNetwork, setCurrentNetwork] = useState<"testnet" | "mainnet">("mainnet");
